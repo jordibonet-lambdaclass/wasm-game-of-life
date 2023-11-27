6b9b6ec40dd05bdcac2f9f0848a4247d77b07355
Jordi <jordi.bonet.valiente@lambdaclass.com>
2023-11-27 17:50:18 +0100

    Add commands in Readme file

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Conway's Game of Life [Rust 🦀 + WASM 🕸️]
 
+## Generate WASM
 
+```sh
+npm init wasm-app web
+```
+
+## Run the web server
+
+```sh
+cd web
+npm cache clean -f
+rm -rf node_modules package-lock.json
+npm i
+npm run start
+```
--- web/index.js
+++ web/index.js
@@ -1,13 +1,28 @@
-import { Universe } from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
 
-const pre = document.getElementById("game-of-life-canvas");
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+// Construct the universe, and get its width and height.
 const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
 
 const renderLoop = () => {
-    pre.textContent = universe.render();
-    universe.tick();
-  
-    requestAnimationFrame(renderLoop);
-};
+  universe.tick();
 
-requestAnimationFrame(renderLoop);
+  drawGrid();
+  drawCells();
+
+  requestAnimationFrame(renderLoop);
+};
