e0e8164adb33e546405db5bc3ec048ccfe35fc4d
Jordi <jordi.bonet.valiente@lambdaclass.com>
2023-11-28 13:46:17 +0100

    Another option for deploying

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
-import { Universe, Cell } from "wasm-game-of-life";
+import { Universe, Cell } from "./pkg/wasm_game_of_life";
 // Import the WebAssembly memory at the top of the file.
-import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+import { memory } from "./pkg/wasm_game_of_life_bg";
 
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#CCCCCC";
